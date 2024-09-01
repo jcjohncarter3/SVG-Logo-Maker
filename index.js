41e6b9d162b8ad038d502faab5aa37f3f9041290
jcjohncarter3 <jcjohncarter3@gmail.com>
2024-08-31 20:34:22 -0500

    Finished shapes.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-
+const {Shape, Circle, Square, Triangle} = require("./lib/shapes")
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -30,8 +30,36 @@ function init() {
     inquirer.prompt (questions).then (answers => {
         console.log (answers)
         
+        // const circle = new Circle(answers.text, answers.textColor, answers.shapeColor);
+        // console.log(circle.render())
+
+        if (answers.shape === "Circle"){
+            const circle = new Circle(answers.text, answers.textColor, answers.shapeColor);
+        console.log(circle.render())
+        writeToFile("logo.svg", circle.render() )
+        } else if (answers.shape === "Square") {
+            const square = new Square(answers.text, answers.textColor, answers.shapeColor);
+            console.log(square.render())
+            writeToFile("logo.svg", square.render() )
+        } else if (answers.shape === "Triangle") {
+            const triangle = new Triangle(answers.text, answers.textColor, answers.shapeColor);
+            console.log(triangle.render())
+            writeToFile("logo.svg", triangle.render() )
+        }
 
     });
  };
 
+ function writeToFile(fileName, data) { 
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          console.error(err);
+        } else {
+          // file written successfully
+        }
+      });
+
+}
+
+ 
  init(); 
